b4f3c861b6be08af8ad5a824f15ecd48fca09954
Yudann <yudannnnn80@gmail.com>
2025-11-02 00:31:10 +0700

    feat: add supabase server

---
+++ src/lib/supabase-server.ts
@@ -0,0 +1,22 @@
+// lib/supabase-server.ts
+import { createClient } from '@supabase/supabase-js';
+import { cookies } from 'next/headers';
+
+export async function createSupabaseServerClient() {
+  const cookieStore = await cookies();
+  
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        persistSession: false,
+      },
+      global: {
+        headers: {
+          Cookie: cookieStore.toString(),
+        },
+      },
+    }
+  );
+}
